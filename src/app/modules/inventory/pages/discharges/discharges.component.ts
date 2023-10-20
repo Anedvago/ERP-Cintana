36f2eb8b9fc444c39a412ceb85887b48d01d7477
Anedvago <61481830+Anedvago@users.noreply.github.com>
2023-10-20 14:30:52 +0000

    Haciendo los layouts para compras y bajas

--- src/app/modules/inventory/pages/discharges/discharges.component.ts
+++ src/app/modules/inventory/pages/discharges/discharges.component.ts
@@ -1,13 +1,44 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormDocumentComponent } from '../../components/form-document/form-document.component';
+import { TableComponent } from 'src/app/shared/table/table.component';
 
 @Component({
   selector: 'app-discharges',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormDocumentComponent, TableComponent],
   templateUrl: './discharges.component.html',
   styleUrls: ['./discharges.component.css']
 })
 export class DischargesComponent {
+  public columnsList = ["date", "number"];
+  public columnsDisplayList = ["Fecha", "Num"];
+  public rowsList = [
+    { date: "2023-03-09", number: "1" },
+    { date: "2023-03-09", number: "2" },
+    { date: "2023-03-09", number: "3" },
+    { date: "2023-03-09", number: "4" },
+    { date: "2023-03-09", number: "1" },
+    { date: "2023-03-09", number: "2" },
+    { date: "2023-03-09", number: "3" },
+    { date: "2023-03-09", number: "4" },
+    { date: "2023-03-09", number: "2" },
+    { date: "2023-03-09", number: "3" },
+    { date: "2023-03-09", number: "4" },
+    { date: "2023-03-09", number: "1" },
+    { date: "2023-03-09", number: "2" },
+    { date: "2023-03-09", number: "3" },
+    { date: "2023-03-09", number: "4" }];
 
+  public columnsDetails = ["ref", "desc", "uni", "value", "discount", "total"];
+  public columnsDisplayDetails = ["Ref", "Descripcion", "Unidades", "Precio", "Descuento", "Total"];
+  public rowsDetails = [
+    { ref: "A001", desc: "Arroz Por Libra", uni: "2", value: "2300", discount: "0", total: "4600" },
+    { ref: "A001", desc: "Arroz Por Libra", uni: "2", value: "2300", discount: "0", total: "4600" },
+    { ref: "A001", desc: "Arroz Por Libra", uni: "2", value: "2300", discount: "0", total: "4600" }];
+
+  public columnsTotals = ["references", "units", "raw", "discount", "total"];
+  public columnsDisplayTotals = ["Refernecias", "Unidades", "Bruto", "Descuento", "Neto"];
+  public rowsTotals = [
+    { references: "2", units: "4", raw: "15600", discount: "0", total: "4600" }];
 }
--- src/app/modules/inventory/pages/purchases/purchases.component.ts
+++ src/app/modules/inventory/pages/purchases/purchases.component.ts
@@ -1,14 +1,46 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormDocumentComponent } from '../../components/form-document/form-document.component';
+import { TableComponent } from 'src/app/shared/table/table.component';
 
 @Component({
   selector: 'app-purchases',
   standalone: true,
-  imports: [CommonModule, FormDocumentComponent],
+  imports: [CommonModule, FormDocumentComponent, TableComponent],
   templateUrl: './purchases.component.html',
   styleUrls: ['./purchases.component.css']
 })
 export class PurchasesComponent {
+  public columnsList = ["date", "number"];
+  public columnsDisplayList = ["Fecha", "Num"];
+  public rowsList = [
+    { date: "2023-03-09", number: "1" },
+    { date: "2023-03-09", number: "2" },
+    { date: "2023-03-09", number: "3" },
+    { date: "2023-03-09", number: "4" },
+    { date: "2023-03-09", number: "1" },
+    { date: "2023-03-09", number: "2" },
+    { date: "2023-03-09", number: "3" },
+    { date: "2023-03-09", number: "4" },
+    { date: "2023-03-09", number: "2" },
+    { date: "2023-03-09", number: "3" },
+    { date: "2023-03-09", number: "4" },
+    { date: "2023-03-09", number: "1" },
+    { date: "2023-03-09", number: "2" },
+    { date: "2023-03-09", number: "3" },
+    { date: "2023-03-09", number: "4" }];
+
+  public columnsDetails = ["ref", "desc", "uni", "value", "discount", "total"];
+  public columnsDisplayDetails = ["Ref", "Descripcion", "Unidades", "Precio", "Descuento", "Total"];
+  public rowsDetails = [
+    { ref: "A001", desc: "Arroz Por Libra", uni: "2", value: "2300", discount: "0", total: "4600" },
+    { ref: "A001", desc: "Arroz Por Libra", uni: "2", value: "2300", discount: "0", total: "4600" },
+    { ref: "A001", desc: "Arroz Por Libra", uni: "2", value: "2300", discount: "0", total: "4600" }];
+
+  public columnsTotals = ["references", "units", "raw", "discount", "total"];
+  public columnsDisplayTotals = ["Refernecias", "Unidades", "Bruto", "Descuento", "Neto"];
+  public rowsTotals = [
+    { references: "2", units: "4", raw: "15600", discount: "0", total: "4600" }];
+
 
 }
