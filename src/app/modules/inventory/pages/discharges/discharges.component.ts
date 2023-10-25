b3a59daf847cb30a385524d5d9397b4ab74bf165
Anedvago <61481830+Anedvago@users.noreply.github.com>
2023-10-25 19:29:20 +0000

    logica para las bajas

--- src/app/modules/inventory/pages/discharges/discharges.component.ts
+++ src/app/modules/inventory/pages/discharges/discharges.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormDocumentComponent } from '../../components/form-document/form-document.component';
 import { TableComponent } from 'src/app/shared/table/table.component';
+import { DischargedService } from 'src/app/services/discharged.service';
 
 @Component({
   selector: 'app-discharges',
@@ -11,34 +12,137 @@ import { TableComponent } from 'src/app/shared/table/table.component';
   styleUrls: ['./discharges.component.css']
 })
 export class DischargesComponent {
-  public columnsList = ["date", "number"];
-  public columnsDisplayList = ["Fecha", "Num"];
-  public rowsList = [
-    { date: "2023-03-09", number: "1" },
-    { date: "2023-03-09", number: "2" },
-    { date: "2023-03-09", number: "3" },
-    { date: "2023-03-09", number: "4" },
-    { date: "2023-03-09", number: "1" },
-    { date: "2023-03-09", number: "2" },
-    { date: "2023-03-09", number: "3" },
-    { date: "2023-03-09", number: "4" },
-    { date: "2023-03-09", number: "2" },
-    { date: "2023-03-09", number: "3" },
-    { date: "2023-03-09", number: "4" },
-    { date: "2023-03-09", number: "1" },
-    { date: "2023-03-09", number: "2" },
-    { date: "2023-03-09", number: "3" },
-    { date: "2023-03-09", number: "4" }];
+  public columnsList = ['date', 'id'];
+  public columnsDisplayList = ['Fecha', 'Num'];
+  public rowsList: any[] = [];
+  public rowsListF: any[] = [];
 
-  public columnsDetails = ["ref", "desc", "uni", "value", "discount", "total"];
-  public columnsDisplayDetails = ["Ref", "Descripcion", "Unidades", "Precio", "Descuento", "Total"];
-  public rowsDetails = [
-    { ref: "A001", desc: "Arroz Por Libra", uni: "2", value: "2300", discount: "0", total: "4600" },
-    { ref: "A001", desc: "Arroz Por Libra", uni: "2", value: "2300", discount: "0", total: "4600" },
-    { ref: "A001", desc: "Arroz Por Libra", uni: "2", value: "2300", discount: "0", total: "4600" }];
+  public columnsDetails = [
+    'reference',
+    'description',
+    'units',
+    'value',
+    'discount',
+    'total',
+  ];
+  public columnsDisplayDetails = [
+    'Ref',
+    'Descripcion',
+    'Unidades',
+    'Precio',
+    'Descuento',
+    'Total',
+  ];
+  public rowsDetails: any[] = [];
 
-  public columnsTotals = ["references", "units", "raw", "discount", "total"];
-  public columnsDisplayTotals = ["Refernecias", "Unidades", "Bruto", "Descuento", "Neto"];
+  public columnsTotals = ['references', 'units', 'gross', 'discount', 'net'];
+  public columnsDisplayTotals = [
+    'Refernecias',
+    'Unidades',
+    'Bruto',
+    'Descuento',
+    'Neto',
+  ];
   public rowsTotals = [
-    { references: "2", units: "4", raw: "15600", discount: "0", total: "4600" }];
+    { references: '2', units: '4', raw: '15600', discount: '0', total: '4600' },
+  ];
+
+  public factureActive: number = 0;
+  constructor(private dischargedService:DischargedService) {
+    this.getListFactures();
+  }
+
+  public getListFactures() {
+    this.dischargedService.getAllDischargesCab().then((data) => {
+      this.rowsList = data!.map(function (obj) {
+        return { id: obj.id, date: obj.date.substring(0, 10) };
+      });
+      this.rowsListF = data!.map(function (obj) {
+        return { id: obj.id, date: obj.date.substring(0, 10) };
+      });
+      this.selectFacture(this.rowsListF[0])
+    });
+  }
+
+  public getTotalsActive() {
+    this.dischargedService.getAllDischargesCab().then((data) => {
+      this.rowsTotals = data!.filter((elem) => {
+        return elem.id == this.factureActive;
+      });
+    });
+  }
+
+  public getDeailsById() {
+    this.dischargedService
+      .getDischargesLinById(this.factureActive)
+      .then((data) => {
+        this.rowsDetails = data!;
+      });
+  }
+
+  public selectFacture(event: any) {
+    this.factureActive = event.id;
+    this.rowsDetails = [];
+    this.rowsTotals = [];
+    this.getTotalsActive();
+    this.getDeailsById();
+  }
+
+  public filterByInitialDate(event: any) {
+    this.rowsListF = this.rowsList.filter((elem) => {
+      return elem.date >= event;
+    })
+    if (this.rowsListF.length > 0) {
+      this.selectFacture(this.rowsListF[0])
+    } else {
+      this.rowsDetails = [];
+      this.rowsTotals = [];
+    }
+  }
+
+  public filterByFinalDate(event: any) {
+    this.rowsListF = this.rowsList.filter((elem) => {
+      return elem.date <= event;
+    })
+    if (this.rowsListF.length > 0) {
+      this.selectFacture(this.rowsListF[0])
+    } else {
+      this.rowsDetails = [];
+      this.rowsTotals = [];
+    }
+  }
+
+  public filterByRangeDate(event: any) {
+    this.rowsListF = this.rowsList.filter((elem) => {
+      return elem.date >= event.initialDate && elem.date <= event.finalDate;
+    })
+    if (this.rowsListF.length > 0) {
+      this.selectFacture(this.rowsListF[0])
+    } else {
+      this.rowsDetails = [];
+      this.rowsTotals = [];
+    }
+
+  }
+  public filterByNumberFacture(event: any) {
+    this.rowsListF = this.rowsList.filter((elem) => {
+      return elem.id == event;
+    })
+    if (this.rowsListF.length > 0) {
+      this.selectFacture(this.rowsListF[0])
+    } else {
+      this.rowsDetails = [];
+      this.rowsTotals = [];
+    }
+  }
+
+  public quitFilters():void{
+    this.rowsListF = this.rowsList;
+    if (this.rowsListF.length > 0) {
+      this.selectFacture(this.rowsListF[0])
+    } else {
+      this.rowsDetails = [];
+      this.rowsTotals = [];
+    }
+  }
 }
--- src/app/services/discharged.service.ts
+++ src/app/services/discharged.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { SupabaseClient, createClient } from '@supabase/supabase-js';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DischargedService {
 
-  constructor() { }
+  private supabaseClient!: SupabaseClient;
+  private supabaseEnvironments: { apiKey: string; url: string } =
+    environment.supabase;
+
+  constructor() {
+    this.supabaseClient = createClient(
+      this.supabaseEnvironments.url,
+      this.supabaseEnvironments.apiKey
+    );
+  }
+
+  public async getAllDischargesCab(): Promise<any[] | null> {
+    let { data: DischargesCab, error } = await this.supabaseClient
+      .from('DischargesCab')
+      .select('*')
+    return DischargesCab;
+  }
+
+  public async getDischargesLinById(id: number): Promise<any[] | null> {
+    let { data: DischargesCab, error } = await this.supabaseClient
+      .from('DischargesLin')
+      .select('*').eq("facture", id)
+    return DischargesCab;
+  }
 }
