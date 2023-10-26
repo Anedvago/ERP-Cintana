51526a4375b41cd1380326c3639ba4e3a59c7a95
Anedvago <61481830+Anedvago@users.noreply.github.com>
2023-10-25 21:58:15 -0500

    calendar

--- src/app/modules/reservations/pages/reservations/reservations.component.ts
+++ src/app/modules/reservations/pages/reservations/reservations.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import { ButtonBlueComponent } from 'src/app/shared/button-blue/button-blue.component';
+import { BookingService } from 'src/app/services/booking.service';
 
 @Component({
   selector: 'app-reservations',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FullCalendarModule, ButtonBlueComponent],
   templateUrl: './reservations.component.html',
-  styleUrls: ['./reservations.component.css']
+  styleUrls: ['./reservations.component.css'],
 })
 export class ReservationsComponent {
+  public reservations: any[] = [];
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    plugins: [dayGridPlugin, timeGridPlugin],
+    eventClick: this.click.bind(this),
+    headerToolbar: {
+      start: 'dayGridMonth,timeGridWeek,timeGridDay',
+      center: 'title',
+      end: 'prevYear,prev,next,nextYear',
+    },
+    nowIndicator: true,
+  };
 
+  constructor(private bookingService: BookingService) {
+    this.bookingService.getAllReservations().then((data) => {
+      this.reservations = data!.map((elem) => {
+        return { title: elem.Customers.name, start: elem.start, end: elem.end };
+      });
+      this.calendarOptions.events = this.reservations;
+    });
+  }
+
+  click() {
+    console.log('click');
+  }
 }
---
+++ src/app/services/booking.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { SupabaseClient, createClient } from '@supabase/supabase-js';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookingService {
+  private supabaseClient!: SupabaseClient;
+  private supabaseEnvironments: { apiKey: string; url: string } =
+    environment.supabase;
+  constructor() {
+    this.supabaseClient = createClient(
+      this.supabaseEnvironments.url,
+      this.supabaseEnvironments.apiKey
+    );
+  }
+
+  public async getAllReservations(): Promise<any[] | null> {
+    let { data: Reservations, error } = await this.supabaseClient
+      .from('Bookings')
+      .select('*,Customers (name)');
+    console.log(Reservations);
+
+    return Reservations;
+  }
+}
